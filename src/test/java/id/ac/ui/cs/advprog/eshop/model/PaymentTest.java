37b78236037ced3dbbf5b1f559e331293c3f1fb5
Amar Hakim <amarhakimhamzah@gmail.com>
2026-03-10 11:24:53 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PaymentTest {
+    @Test
+    void testCreatePayment() {
+        Map<String, String> data = new HashMap<>();
+        data.put("voucherCode", "ESHOP1234ABC5678");
+
+        Payment payment = new Payment("1", "VOUCHER", data);
+
+        assertEquals("1", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals(data, payment.getPaymentData());
+    }
+}
